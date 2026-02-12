e6b14f2b614b8424bfcf5368b060bd8e4d444a4f
Suhrita Reddy <suhritareddy5@gmail.com>
2026-02-12 14:53:03 +0530

    fix import issues for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
-import Home from './pages/home'
+import Home from './pages/Home'
 import Doctor from './pages/Doctor'
 import Login from './pages/Login'
 import About from './pages/About'
